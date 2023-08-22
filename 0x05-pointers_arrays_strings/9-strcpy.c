36f291e49e095d24e55a194643b282e1d435b72f
codealittlemore <adebayokokumo@gmail.com>
2023-08-21 21:57:44 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies
+ * @dest: char type
+ * @src: char type
+ * Description: place
+ * Return: points tp
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int z = -1;
+
+	do(
+		z++;
+		dest[z] = src[z];
+	) while (src[z] != '\0');
+
+	return (dest);
+}
