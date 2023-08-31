85c9a1e803d90d0593741096619dbadafa9a1482
codealittlemore <adebayokokumo@gmail.com>
2023-08-30 17:18:52 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - funtion
+ * @n: value
+ * Return: new value
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - function
+ * @n: value
+ * @i: interation
+ * Return: result
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i + i > n)
+	return (-1);
+	if (i + i == n)
+	return (i);
+	return (actual_sqrt_recursion(n, n + i));
+}
