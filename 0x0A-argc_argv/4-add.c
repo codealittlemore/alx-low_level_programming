1c429ecf514cf839cccc5f5c139c9769ebb39920
codealittlemore <adebayokokumo@gmail.com>
2023-09-02 03:41:15 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,69 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
- * check_num - check - string
- * @str: array str
- * Return: always 0
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
  */
-
-int check_num(char *str)
+int main(int argc, char **argv)
 {
-	/*Declaring variables*/
-	unsigned int count;
+	int i, n, sum = 0;
+	char *flag;
 
-	count = 0;
-	while (count < strlen(str)) /*count string*/
-	
+	if (argc < 2)
 	{
-	if (!isdigit(str[count])) /*check*/
-	{
+	printf("0\n");
 	return (0);
 	}
 
-	count++;
-	}
-	return (1);
-}
-
-/**
- * main - function
- * @argc: count
- * @argv: arguments
- * Return: always 0
- */
-
-int main(int argc, char *argv[])
-{
-	/*Declaring variables*/
-	int count;
-	int str_to_int;
-	int sum = 0;
-
-	count = 1;
-	while (count < argc) /*array*/
-	
+	for (i = 1; argv[i]; i++)
 	{
-	if (check_num(argv[count]))
-	
-	{
-	str_to_int = atoi(argv[count]); /*a*/
-	sum += str_to_int;
-	}
-
-	/*condition*/
-	else
+	n = strtol(argv[i], &flag, 10);
+	if (*flag)
 	{
 	printf("Error\n");
 	return (1);
 	}
-
-	count++;
+	else
+	{
+	sum += n;
 	}
+	}
+	printf("%d\n", sum);
 
-	printf("%d\n", sum); /*sum*/
-	
 	return (0);
 }
